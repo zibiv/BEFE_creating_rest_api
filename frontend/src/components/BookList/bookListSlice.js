20443a3d1e9cc712fdfbbc33910129447b111b7b
GenIl <topgear09@yandex.ru>
2023-03-22 17:17:49 +0300

    Добавлен reducer для получения всех книг из API Создан thunk action creator Селектор для получения списка всех книг

--- frontend/src/components/BookList/bookListSlice.js
+++ frontend/src/components/BookList/bookListSlice.js
@@ -1,4 +1,13 @@
 import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
+import { getBooks as fetchBooks } from "../../api/books";
+
+export const getBooksAction = () => {
+  return async(dispatch) => {
+    const books = await fetchBooks();
+    dispatch({type: 'bookList/getBooks', payload: books});
+  }
+}
+
 
 const bookListSlice = createSlice({
   name: 'bookList',
@@ -6,7 +15,19 @@ const bookListSlice = createSlice({
     books: [],
     isLoading: false,
     hasError: false
+  },
+  reducers: {
+    getBooks: (state, action) => {
+        console.log('I in reduser');
+        state.books = action.payload;
+    } 
   }
 });
+
+
+
+export const selectAllBooks = state => {
+  return state.bookList.books;
+}
 
 export default bookListSlice.reducer;
