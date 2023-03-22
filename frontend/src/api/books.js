3dcd96562ca871c3032fe4caf11c53d0e875d62d
GenIl <topgear09@yandex.ru>
2023-03-22 12:17:58 +0300

    Добавлен запрос для изменения названия книги из компонента Book

--- frontend/src/api/books.js
+++ frontend/src/api/books.js
@@ -23,4 +23,20 @@ export const addNewBook = async (newBookTitle, newBookStart, newBookEnd) => {
 
 // TODO: Create the updateBook function that takes the arguments id, newTitle, newStart, newEnd. Inside of the function, create a PUT request for the specified book to be updated. Return the status of the response at the end of the function.
 
+const updateBook = async (id, newTitle, newStart, newEnd) => {
+  const response = await fetch(`${BOOKS_API}/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    }, 
+    body: JSON.stringify({
+      newTitle, 
+      newStart, 
+      newEnd
+    })
+  })
+  //возвращаем статус операции, в компоненте статус будет проверен, если статус будет отличаться от 200 то обработчик события в компоненте выведет сообщение для пользователя
+  return response.status;
+}
+
 // TODO: Create the deleteBook function that takes the id of the book to be deleted as an argument. Inside of the function, create a DELETE request for the specified book to be deleted. Return the status of the response at the end of the function.
