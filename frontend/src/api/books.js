77ac30d1ef91a6ba7915075d8f2c39c743c85388
GenIl <topgear09@yandex.ru>
2023-03-22 12:12:07 +0300

    Добавлен запрос для добавления новой книги

--- frontend/src/api/books.js
+++ frontend/src/api/books.js
@@ -1,7 +1,24 @@
 // TODO: Import API_ENDPOINT
-
+import { API_ENDPOINT } from './';
+const BOOKS_API = API_ENDPOINT + '/books';
 // TODO: Create the addNewBook function that takes in newTitle, newStart, and newEnd as arguments. Inside the function, create a POST request for the new book. Store the response as a JSON in a variable called newBook and return it at the end of the function.
+export const addNewBook = async (newBookTitle, newBookStart, newBookEnd) => {
+  const respond = await fetch(BOOKS_API, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      title: newBookTitle,
+      start: newBookStart,
+      end: newBookEnd,
+    }),
+  });
 
+  const newBookJson = await respond.json();
+  return newBookJson;
+
+};
 // TODO: Create the getBooks function that retrieves all of the books that have been saved to the back-end API
 
 // TODO: Create the updateBook function that takes the arguments id, newTitle, newStart, newEnd. Inside of the function, create a PUT request for the specified book to be updated. Return the status of the response at the end of the function.
